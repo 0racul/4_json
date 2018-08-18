d7b2fd6463da1945757f66d3472df6f6d898de8b
0racul <nightcrowl@yandex.ru>
2018-08-18 21:21:23 +0700

    1 version 4_json

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,25 @@ import json
 
 
 def load_data(filepath):
-    pass
+
+    with open(filepath) as f:
+
+        json.loads(f.read())
+
+    with open("pretty.json", "w") as g:
+
+        json.dump(f, g, indent=5)
+
+    return f
 
 
-def pretty_print_json(data):
-    pass
+print("file path pls \n")
+
+dir_path = input()
+
+loaded_data = load_data(dir_path)
 
 
-if __name__ == '__main__':
-    pass
+
+
+
