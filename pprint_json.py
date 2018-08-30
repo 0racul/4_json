8367279b9565cb861c75485530f538179c04e0ad
0racul <nightcrowl@yandex.ru>
2018-08-30 21:42:21 +0700

    Post_fix 23.aug Var_names rename, parameters add

--- pprint_json.py
+++ pprint_json.py
@@ -1,1 +1,33 @@
-/home/inverdat/PycharmProjects/4_json/pprint_json.py
+import json
+import argparse
+
+def load_data(filepath):
+
+    with open(filepath) as file:
+        decoded_json = json.loads(file.read())
+
+    return decoded_json
+
+
+def print_data(json_data):
+
+    pretty_json = json.dumps(json_data, indent=4, sort_keys=True)
+    print(pretty_json)
+
+    return
+
+
+def parser_parser():
+    global parser
+    parser = argparse.ArgumentParser()
+    pars_result = parser.parse_args()
+
+    return vars(pars_result)
+
+
+if __name__ == "__main__":
+
+    pars_result = parser_parser()
+    dir_path = pars_result.get("path_to_file")
+    loaded_data = load_data(dir_path)
+    print_data(loaded_data)
